72a04358148d70ef23db6291912adbeefaecfe2e
= <=>
2021-11-18 12:11:03 +0900

    룰렛 돌리기 추가

--- go.mod
+++ go.mod
@@ -8,9 +8,10 @@ require (
 	github.com/bwmarrin/discordgo v0.23.2
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/jipark0716/typecastGo v0.0.0-20211028233909-24b1d5b918d2
+	github.com/jipark0716/typecastGo v0.0.0-20211111042157-e5e42b42f441
 	github.com/jmoiron/sqlx v1.3.4
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/sys v0.0.0-20211107104306-e0b2ad06fe42 // indirect
+	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
+	golang.org/x/net v0.0.0-20211109214657-ef0fda0de508 // indirect
+	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
 )
--- main.go
+++ main.go
@@ -8,16 +8,17 @@ import (
 	"sync"
 	"unicode/utf8"
 	"os"
+	"math/rand"
 	"io/ioutil"
 
 	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
-	"github.com/jipark0716/typecastGo"
+	typecastGo "github.com/jipark0716/typecastGo"
 )
 
 const (
 	HELP_MAIN = "- &목소리 목소리 설정\n- &{할말} TTS가 읽어줌\n참고 https://github.com/jipark0716/discordTypecast"
-	HELP_목소리= "- &목소리 {이름} 하면 설정\n목소리 지원 \n찬구, 주하, 카밀라, 우주, 혜정, 한유격, 영희, 소영, 지영 경완, 성욱, 애란, 줄리아, 민지, 신혜, 준상, 현경, 성규 윤성, 관용, 정순, 다희, 경숙, 동우, 연길, 지우, 나진, 쥬비 정희, 연우, 용호, 재훈, 정섭, 상도, 진혁, 민상, 찬혁, 지철 수진, 국희, 제임스, 선영, 금희, 명희, 미오, 보노, 덕춘 강수정, 이승주, 주원, 의찬, 마크, 트럼프, 김정은, 문재인 신디, 현진, 정원, 성배, 리춘희, 하은, 범수"
+	HELP_목소리= "- &목소리 {이름} 하면 설정\n목소리 지원 확인 https://github.com/jipark0716/typecastGo#%EB%AA%A9%EC%86%8C%EB%A6%AC-%EC%A7%80%EC%9B%90"
 	ERROR_목소리 = "지원하지 않는 목소리 입니다. 지원하는 목소리를 확인하려면 \"&목소리 도움\"을 입력하세요"
 	COMMENT_NOTFOUND = "지원하지 않는 명령어입니다. 도움말이 확인하려면 \"&도움\"을 입력하세요."
 )
@@ -26,6 +27,7 @@ var typecast typecastGo.TypeCast
 var voiceMutex map[string]*sync.Mutex
 
 func init() {
+	rand.Seed(time.Now().UnixNano())
 	voiceMutex = make(map[string]*sync.Mutex)
 	dgvoice.OnError = func(str string, err error) {
 		// println(str)
@@ -71,6 +73,8 @@ func onCreateMessage(discord *discordgo.Session, message *discordgo.MessageCreat
 		return
 	} else if strings.HasPrefix(message.Content, "&도움") {
 		discord.ChannelMessageSend(message.ChannelID, HELP_MAIN)
+	} else if strings.HasPrefix(message.Content, "&룰렛") {
+		roulette(discord, message)
 	} else if strings.HasPrefix(message.Content, "&목소리 도움") {
 		discord.ChannelMessageSend(message.ChannelID, HELP_목소리)
 	} else if strings.HasPrefix(message.Content, "&벙어리") {
@@ -100,6 +104,47 @@ func settingActor(discord *discordgo.Session, message *discordgo.MessageCreate, 
 	} else {
 		discord.ChannelMessageSend(message.ChannelID, "변경실패")
 	}
+}
+
+func roulette(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	state, err := discord.State.VoiceState(message.GuildID, message.Author.ID)
+	if err != nil {
+		discord.ChannelMessageSend(message.ChannelID, "음성채널에 먼저 입장해주세요.")
+		return
+	}
+
+	members := []*discordgo.Member{}
+
+	guild, err := discord.State.Guild(message.GuildID)
+	for _, s := range guild.VoiceStates {
+		if s.ChannelID == state.ChannelID {
+			for _, block := range message.Message.Mentions {
+				if s.UserID == block.ID {
+					goto ROULETTE_BLOCK
+				}
+			}
+			member, err := discord.State.Member(message.GuildID, s.UserID)
+			if err == nil && !member.User.Bot {
+				members = append(members, member)
+			}
+		}
+		ROULETTE_BLOCK:
+	}
+	if len(members) < 2 {
+		discord.ChannelMessageSend(message.ChannelID, "참가자가 두명이상이여야 합니다.")
+		return
+	}
+	prevMessage := "참가자 : "
+	for _, member := range members {
+		prevMessage += member.Mention()+" "
+	}
+	discord.ChannelMessageSend(message.ChannelID, prevMessage)
+
+	rand.Shuffle(len(members), func(i int, j int) {
+		members[i], members[j] = members[j], members[i]
+	})
+
+	discord.ChannelMessageSend(message.ChannelID, "당첨 : "+members[0].Mention())
 }
 
 func muteToggle(discord *discordgo.Session, message *discordgo.MessageCreate) {
