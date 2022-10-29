85cc10fbfbda676244dfeef600394bf0700a1cef
jipark <jipark0716@gmail.com>
2022-10-29 17:10:09 +0900

    feat: 에러로그 file로 출력
    1. 에러로그 file로 출력
    2. log 디렉토리 이그노어 추가

--- handler/report.go
+++ handler/report.go
@@ -2,11 +2,26 @@ package handler
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/pkg/errors"
 )
 
+var FileLogger *log.Logger
+
+func init() {
+	os.MkdirAll("log", os.ModePerm)
+
+	file, err := os.OpenFile("log/discord_typecast.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("%+v", err)
+	}
+
+	FileLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
 func Report(err error) {
 	err = errors.Wrap(err, "report error")
-	fmt.Printf("%+v\n", err)
+	FileLogger.Println(fmt.Sprintf("%+v\n", err))
 }
